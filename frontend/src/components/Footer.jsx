225d72da45e045581e11c1ff81bf2cfb32f115a4
0xhiro <0xhiro@tutamail.com>
2024-04-15 05:07:20 -0400

    Add LLC name for Stripe compliance

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
   return (
     <footer className="footer-container text-center p-3">
-        <span>© {new Date().getFullYear()} All rights reserved.</span>
+        <span>© {new Date().getFullYear()} Tyrconnell LLC. All rights reserved.</span>
         <div className="footer-links">
           <a href="https://restaked-app.gitbook.io/restaked.app-api-documentation/terms-of-service">Terms of Service</a>
         </div>
